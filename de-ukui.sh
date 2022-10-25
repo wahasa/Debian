0a55563ff134bff6552ace3ceb871819f327614c
wahasa <69626847+wahasa@users.noreply.github.com>
2022-10-25 08:47:04 +0700

    Add files via upload

---
+++ de-ukui.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#Get the necessary components
+apt-get update
+apt-get install udisks2 -y
+echo " " > /var/lib/dpkg/info/udisks2.postinst
+apt-mark hold udisks2
+apt-get install sudo tzdata -y
+apt-get install ukui* libukui* libqt5-ukui* kylin-display-switch kylin-nm peony -y
+apt-get install tigervnc-standalone-server dbus-x11 -y
+apt-get --fix-broken install
+apt-get clean
+
+#Setup the necessary files
+mkdir -p ~/.vnc
+echo "#!/bin/bash
+export PULSE_SERVER=127.0.0.1
+xrdb $HOME/.Xresources
+ukui-session" > ~/.vnc/xstartup
+
+echo "#!/bin/sh
+rm -rf /run/dbus/pid
+dbus-daemon --system
+dbus-launch
+DISPLAY=:1 $HOME/.vnc/xstartup" > /usr/local/bin/vncstart
+
+echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
+echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
+clear
+chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/vncstart
+chmod +x /usr/local/bin/vnc-start
+chmod +x /usr/local/bin/vnc-stop
+
+echo " "
+echo "Installing browser,.."
+echo " "
+sudo apt install firefox-esr -y
+clear
+echo " "
+echo "Vnc Server address will run at 127.0.0.1:5901"
+echo " "
+echo "Start Vnc Server, run vnc-start"
+echo " "
+echo "Stop Vnc Server, run vnc-stop"
+echo " "
+
+rm de-ukui.sh
+
+vnc-start
