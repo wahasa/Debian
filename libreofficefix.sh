2a2350aab7bc5bf15d0c24971c1515c41b27474a
wahasa <69626847+wahasa@users.noreply.github.com>
2022-10-26 13:38:48 +0700

    Create libreofficefix.sh

---
+++ libreofficefix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install libreoffice -y
+rm -rf /usr/lib/libreoffice/program/oosplash
+
+wget -q https://github.com/wahasa/Ubuntu/blob/main/Patch/oosplash?raw=true -O /usr/lib/libreoffice/program/oosplash
+
+chmod +x /usr/lib/libreoffice/program/oosplash
+mkdir /prod && mkdir /prod/version
+
+echo " "
+echo "Patch has been applied successfully"
+echo " "
+
+rm libreofficefix.sh
