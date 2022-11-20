5ff224b0032623ae1cb7e93332e192e7db8dc9e8
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-20 09:10:17 +0700

    Update debian-buster-armhf.sh

--- Rootfs/debian-buster-armhf.sh
+++ Rootfs/debian-buster-armhf.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+wget "https://github.com/debuerreotype/docker-debian-artifacts/blob/905330be49c868aeb3985d5746873aa8d24c0e70/buster/rootfs.tar.xz?raw=true" -O debian-rootfs.tar.xz
+
+rm debian-buster-armhf.sh
 
