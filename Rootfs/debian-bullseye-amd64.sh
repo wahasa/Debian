3a0581b8a0e1c214ace4c3a53d6cd11303c83a7a
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-20 09:25:52 +0700

    Update debian-bullseye-amd64.sh

--- Rootfs/debian-bullseye-amd64.sh
+++ Rootfs/debian-bullseye-amd64.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 wget "https://github.com/debuerreotype/docker-debian-artifacts/blob/72073f82c7f45bd4cee0b2daf125ceaac10d2a97/bullseye/rootfs.tar.xz?raw=true" -O debian-rootfs.tar.xz
-
 rm debian-bullseye-amd64.sh
