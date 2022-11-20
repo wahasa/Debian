fe4056d6946a35e395af5089fb6c4e4a7d8fca6a
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-20 08:51:47 +0700

    Update debian-bullseye-amd64.sh

--- Rootfs/debian-bullseye-amd64.sh
+++ Rootfs/debian-bullseye-amd64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget "https://github.com/wahasa/Debian/releases/download/11.5/Debian-11.5-${archurl}-bullseye.tar.xz" -O debian-rootfs.tar.xz
+wget "https://github.com/debuerreotype/docker-debian-artifacts/blob/72073f82c7f45bd4cee0b2daf125ceaac10d2a97/bullseye/rootfs.tar.xz?raw=true" -O debian-rootfs.tar.xz
 
 rm debian-bullseye-amd64.sh
