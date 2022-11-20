508a2aad92fa9e130a1af17352d0443518e2a20a
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-20 09:13:24 +0700

    Update debian-bookworm-amd64.sh

--- Rootfs/debian-bookworm-amd64.sh
+++ Rootfs/debian-bookworm-amd64.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+wget "https://github.com/debuerreotype/docker-debian-artifacts/blob/72073f82c7f45bd4cee0b2daf125ceaac10d2a97/bookworm/rootfs.tar.xz?raw=true" -O debian-rootfs.tar.xz
 
+rm debian-bookworm-amd64.sh
