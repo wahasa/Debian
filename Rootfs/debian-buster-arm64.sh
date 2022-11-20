a9ea4c5a6ea5966019ce1da574a58772cff754fd
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-20 09:09:07 +0700

    Update debian-buster-arm64.sh

--- Rootfs/debian-buster-arm64.sh
+++ Rootfs/debian-buster-arm64.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+wget "https://github.com/debuerreotype/docker-debian-artifacts/blob/fc8328761df3015fe8bbe9fded92cb050e2c59fc/buster/rootfs.tar.xz?raw=true" -O debian-rootfs.tar.xz
 
+rm debian-buster-arm64.sh
