362d13a2e24787511c13ce10657e97767086666a
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-20 09:25:34 +0700

    Update debian-bookworm-armhf.sh

--- Rootfs/debian-bookworm-armhf.sh
+++ Rootfs/debian-bookworm-armhf.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 wget "https://github.com/debuerreotype/docker-debian-artifacts/blob/905330be49c868aeb3985d5746873aa8d24c0e70/bookworm/rootfs.tar.xz?raw=true" -O debian-rootfs.tar.xz
-
 rm debian-bookworm-armhf.sh
-
