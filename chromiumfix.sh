e576b5c54a093dad333e450727987ee91515cb22
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-19 09:41:12 +0700

    Update chromiumfix.sh

--- chromiumfix.sh
+++ chromiumfix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt update && apt upgrade -y
-sudo apt install chromium -y
+apt update
+apt install chromium -y
 
 sed -i 's/chromium %U/chromium --no-sandbox --test-type %U/g' /usr/share/applications/chromium.desktop
 
