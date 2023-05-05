cad56a3d62ff50f229027f10368b70f59fe69077
wahasa <69626847+wahasa@users.noreply.github.com>
2023-05-05 10:27:14 +0700

    Update de-cinnamon.sh

--- de-cinnamon.sh
+++ de-cinnamon.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install udisks2 -y
 echo " " > /var/lib/dpkg/info/udisks2.postinst
 apt-mark hold udisks2
-apt-get install sudo tzdata -y
+apt-get install sudo -y ; dpkg-reconfigure tzdata
 apt-get install cinnamon-desktop-environment -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
 apt-get --fix-broken install
