fce07b2d0f188d7c62c2e8e12168e69f376ed22b
wahasa <69626847+wahasa@users.noreply.github.com>
2023-05-05 10:24:41 +0700

    Update and rename Problem/de-mate.sh to de-mate.sh

---
+++ de-mate.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#Get the necessary components
+apt-get update
+apt-get install udisks2 -y
+echo " " > /var/lib/dpkg/info/udisks2.postinst
+apt-mark hold udisks2
+apt-get install sudo -y ; dpkg-reconfigure tzdata
+apt-get install mate-desktop-environment mate-terminal -y
+apt-get install tigervnc-standalone-server dbus-x11 -y
+apt-get --fix-broken install
+apt-get clean
+
+#Setup the necessary files
+mkdir -p ~/.vnc
+echo "#!/bin/bash
+export PULSE_SERVER=127.0.0.1
+xrdb $HOME/.Xresources
+mate-session" > ~/.vnc/xstartup
+
+echo "#!/bin/sh
+export DISPLAY=:1
+export PULSE_SERVER=127.0.0.1
+rm -rf /run/dbus/dbus.pid
+dbus-launch mate-session" > /usr/local/bin/vncstart
+   echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
+   echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
+   chmod +x ~/.vnc/xstartup
+   chmod +x /usr/local/bin/*
+   clear
+   echo ""
+   echo "Installing Browser,.."
+   echo ""
+   apt install firefox-esr -y
+   clear
+   echo ""
+   echo "Vnc Server address will run at 127.0.0.1:5901"
+   echo "Start Vnc Server, run vnc-start"
+   echo "Stop Vnc Server, run vnc-stop"
+   echo ""
+rm de-mate.sh
+#
